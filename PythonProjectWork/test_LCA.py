12ffff723dc766a754decbb35cf513e47138a96f
AaronReadman <readmana@tcd.ie>
2021-10-03 20:08:10 +0100

    Basic Unit test for Python LCA code

---
+++ PythonProjectWork/test_LCA.py
@@ -0,0 +1,28 @@
+import unittest
+from LCA import Node, findLCA
+import LCA
+
+
+class TestingLCA(unittest.TestCase):
+#Building a basic binary tree and then testing to see if it finds the correct ancestor
+    def test_basicGraph(self):
+        #assert(False)
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
+        root.right.right=Node(3)
+        root.right.right.right=Node(4)
+        root.left.left=Node(5)
+        root.left.right=Node(6)
+
+        result=findLCA(root,5,4)
+        self.assertEqual(result,1)
+        
+
+if __name__=='__main__':
+    unittest.main()        
+
+
+
+
+
