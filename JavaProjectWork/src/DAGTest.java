86737924a0bbaf4422facd1cabab7f34aac94cfb
AaronReadman <readmana@tcd.ie>
2021-10-21 20:26:16 +0100

    Fixed some of my tests because they kept giving index out of bounds errors

--- JavaProjectWork/src/DAGTest.java
+++ JavaProjectWork/src/DAGTest.java
@@ -7,30 +7,30 @@ public class DAGTest {
 	@Test
 	public void testCreateDAG()
 	{
-		DAG dag1 = new DAG(4);
+		DAG dag1 = new DAG(5);
 						
 		//Testing a Normal  Binary Tree Graph
-		assertEquals("if Dag successfully adds node 2 to 1 then it should return true",true,dag1.add(1,2));
+		assertEquals("if Dag successfully adds node 2 to 1 then it should return true",true,dag1.add(0,1));
+		dag1.add(0,2);
 		dag1.add(1,3);
-		dag1.add(3,1);
-		dag1.add(3,4);
+		dag1.add(2,4);
 		
 		
 		//Testing a Graph construction where each Node only has one child
-		dag1 = new DAG(4);
+		dag1 = new DAG(5);
 	
-		dag1.add(1,2);	
+		dag1.add(0,1);	
+		dag1.add(1,2);
 		dag1.add(2,3);
 		dag1.add(3,4);
-		dag1.add(4,5);
 		
 		//Testing a Graph construction where a node has multiple child Nodes
-		dag1 = new DAG(4);
+		dag1 = new DAG(5);
 	
-		dag1.add(1,2);	
-		dag1.add(1,3);
-		dag1.add(1,4);
-		dag1.add(1,5);
+		dag1.add(0,1);	
+		dag1.add(0,2);
+		dag1.add(0,3);
+		dag1.add(0,4);
 		
 		
 		assert(true);
@@ -39,24 +39,39 @@ public class DAGTest {
 	}
 	
 	@Test
+	//Testing for Cycles occuring in the graph just to make sure its acyclic
+	public void testCycle()
+	{
+		
+		DAG dag1 = new DAG(5);
+		
+		dag1.add(0,1);	
+		dag1.add(0,2);
+		dag1.add(2,3);
+		dag1.add(2,4);
+		
+	}
+	
+	@Test
 	//Testing for LCA base cases
 	public void DAGLCATest()
 	{
 		
-		DAG dag1 = new DAG(4);
+		DAG dag1 = new DAG(5);
 		
-		dag1.add(1,2);	
-		dag1.add(1,3);
-		dag1.add(3,1);
-		dag1.add(3,4);
+		dag1.add(0,1);	
+		dag1.add(0,2);
+		dag1.add(2,3);
+		dag1.add(2,4);
 		
-		assertEquals(3,dag1.getLCA(1,4));
-		assertEquals(1,dag1.getLCA(2,4));
+		assertEquals(2,dag1.getLCA(4,3));
+		assertEquals(0,dag1.getLCA(1,4));
 		
 		
 		//Testing a Graph construction where each Node only has one child
-		dag1 = new DAG(4);
+		dag1 = new DAG(6);
 	
+		dag1.add(0, 1);
 		dag1.add(1,2);	
 		dag1.add(2,3);
 		dag1.add(3,4);
