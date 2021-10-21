4a4f9dcbaef8d15726f98fadf1a6662eb271f90e
AaronReadman <readmana@tcd.ie>
2021-10-21 18:15:40 +0100

    minor edit to Dag Test

--- JavaProjectWork/src/DAGTest.java
+++ JavaProjectWork/src/DAGTest.java
@@ -9,11 +9,14 @@ public class DAGTest {
 	{
 		DAG dag1 = new DAG();
 						
+
+		assert("if Dag successfully adds node 2 to 1 then it should return true",true,dag1.add(1,2));
 		
-		dag1.add(1,2);
 		dag1.add(1,3);
 		dag1.add(3,1);
 		dag1.add(3,4);
+		
+		
 
 	}
 
