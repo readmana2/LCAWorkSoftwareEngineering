7a163d33d86249a0db18ab1b616c2f840f1219c1
AaronReadman <readmana@tcd.ie>
2021-10-21 23:31:10 +0100

    testing LCA with more Graphs

--- JavaProjectWork/src/DAGTest.java
+++ JavaProjectWork/src/DAGTest.java
@@ -74,11 +74,11 @@ public class DAGTest {
 		dag1.add(2,3);
 		dag1.add(2,4);
 		
-		assertEquals(2,dag1.getLCA(4,3));
+		assertEquals("testing for ordinary binary tree",2,dag1.getLCA(4,3));
 		
 		
 		
-		assertEquals(0,dag1.getLCA(1,4));
+		assertEquals("testing again for when the two nodes are more separated",0,dag1.getLCA(1,4));
 		
 		
 		//Testing a Graph construction where each Node only has one child
@@ -90,7 +90,24 @@ public class DAGTest {
 		dag1.add(3,4);
 		dag1.add(4,5);
 		
-		assertEquals(2,dag1.getLCA(2,5));
+		assertEquals("Only 1 link connecting each point",2,dag1.getLCA(2,5));
+		
+		
+		dag1 = new DAG(10);
+		
+		dag1.add(0, 1);
+		dag1.add(0,2);	
+		dag1.add(0,3);
+		dag1.add(3,4);
+		dag1.add(3,5);
+		
+		dag1.add(3, 6);
+		dag1.add(3,7);	
+		dag1.add(1,8);
+		dag1.add(7,8);
+		dag1.add(8,9);
+		
+		assertEquals(1,dag1.getLCA(9,1));
 		
 		
 		
