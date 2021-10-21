2be5b77a86371b74cef69bf7172f94edd229faa9
AaronReadman <readmana@tcd.ie>
2021-10-21 18:12:57 +0100

    Added a short test for what should happen when creating a DAG

--- JavaProjectWork/src/DAGTest.java
+++ JavaProjectWork/src/DAGTest.java
@@ -5,8 +5,16 @@ import org.junit.Test;
 public class DAGTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreateDAG()
+	{
+		DAG dag1 = new DAG();
+						
+		
+		dag1.add(1,2);
+		dag1.add(1,3);
+		dag1.add(3,1);
+		dag1.add(3,4);
+
 	}
 
 }
